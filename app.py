a71b3931b0fe5e7e782d02c2f3b2c7bf86c14dbc
kknownymouss <unknownpythonic@gmail.com>
2022-07-02 19:30:34 +0300

    setting flask

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 app.secret_key = "hello" 
 socketio = SocketIO(app)
 news = []
-db = SQLAlchemy(app)
 
 
 
@@ -16,23 +15,24 @@ def register_name():
         session["name"] = request.form["name"]
         return redirect(url_for("chatroom"))
     else:
-        return render_template("register.html")
+        return render_template("login.html")
 
 
-app.route("/chatroom", mathods=["POST", "GET"])
+@app.route("/chatroom", methods=["POST", "GET"])
 def chatroom():
     if request.method == "POST":
         pass
-    
-    if request.method == "GET":
-        return render_template("chatroom.html")
+
+    else:
+        print(session["name"])
+        return render_template("ChatRoom.html")
 
 @socketio.on('my event')
 def handle_my_custom_event(json):
     if 'message' in json:
         if json['message'] != "":
             if json['message'] == '#pw01':
-                json['message'] = session['user-name'] + " connected"
+                json['message'] = session['name'] + " connected"
                 json['username'] = "Local Bot"
                 socketio.emit('my response', json, namespace="/")
             else:
@@ -40,3 +40,6 @@ def handle_my_custom_event(json):
                 socketio.emit('my response', json)
     else:
         pass
+
+if __name__ == "__main__":
+    app.run()
